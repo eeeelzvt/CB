b28bf429a1c284fc2a45eba7e7a14e11d6692906
TheSuspect17 <91951764+TheSuspect17@users.noreply.github.com>
2022-11-19 13:14:41 +0300

    Add files via upload

--- recomendational_system.py
+++ recomendational_system.py
@@ -6,7 +6,7 @@ from seaborn import regression
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 
-n = 3
+n = 6
 
 usd_rub_day = pd.read_csv(
     "https://query1.finance.yahoo.com/v7/finance/download/RUB=X?period1=1652918400&period2=1668816000&interval=1d&events=history&includeAdjustedClose=true")
@@ -29,10 +29,31 @@ euro_usd_week = pd.read_csv(
 euro_usd_month = pd.read_csv(
     "https://query1.finance.yahoo.com/v7/finance/download/EURUSD=X?period1=1637289209&period2=1668825209&interval=1mo&events=history&includeAdjustedClose=true")
 
+funt_rub_day = pd.read_csv(
+    "https://query1.finance.yahoo.com/v7/finance/download/GBPRUB=X?period1=1652918400&period2=1668816000&interval=1d&events=history&includeAdjustedClose=true")
+funt_rub_week = pd.read_csv(
+    "https://query1.finance.yahoo.com/v7/finance/download/GBPRUB=X?period1=1652918400&period2=1668816000&interval=1wk&events=history&includeAdjustedClose=true")
+funt_rub_month = pd.read_csv(
+    "https://query1.finance.yahoo.com/v7/finance/download/GBPRUB=X?period1=1652918400&period2=1668816000&interval=1mo&events=history&includeAdjustedClose=true")
+
+funt_usd_day = pd.read_csv(
+    "https://query1.finance.yahoo.com/v7/finance/download/GBPUSD=X?period1=1652918400&period2=1668816000&interval=1d&events=history&includeAdjustedClose=true")
+funt_usd_week = pd.read_csv(
+    "https://query1.finance.yahoo.com/v7/finance/download/GBPUSD=X?period1=1652918400&period2=1668816000&interval=1wk&events=history&includeAdjustedClose=true")
+funt_usd_month = pd.read_csv(
+    "https://query1.finance.yahoo.com/v7/finance/download/GBPUSD=X?period1=1652918400&period2=1668816000&interval=1mo&events=history&includeAdjustedClose=true")
+
+funt_euro_day = pd.read_csv(
+    "https://query1.finance.yahoo.com/v7/finance/download/GBPEUR=X?period1=1652918400&period2=1668816000&interval=1d&events=history&includeAdjustedClose=true")
+funt_euro_week = pd.read_csv(
+    "https://query1.finance.yahoo.com/v7/finance/download/GBPEUR=X?period1=1652918400&period2=1668816000&interval=1wk&events=history&includeAdjustedClose=true")
+funt_euro_month = pd.read_csv(
+    "https://query1.finance.yahoo.com/v7/finance/download/GBPEUR=X?period1=1652918400&period2=1668816000&interval=1mo&events=history&includeAdjustedClose=true")
+
 money = [usd_rub_day, usd_rub_week, usd_rub_month, euro_rub_day, euro_rub_week, euro_rub_month, euro_usd_day,
-         euro_usd_week, euro_usd_month]
+         euro_usd_week, euro_usd_month, funt_rub_day, funt_rub_week, funt_rub_month,funt_usd_day,funt_usd_week,funt_usd_month,funt_euro_day,funt_euro_week,funt_euro_month]
 most_v = []
-name_money = ['usd->rub', 'euro->rub', 'euro->usd']
+name_money = ['usd->rub', 'euro->rub', 'euro->usd', 'funt->rub', 'funt->usd','funt->euro']
 
 for i in range(3 * n):
     data = money[i]
@@ -67,6 +88,3 @@ for i in range(3 * n):
 print('Самая выгодный перевод на следующую неделю: ', name_money[np.array(most_v[::3]).argmax()])
 print('Самая выгодный перевод на следующий месяц: ', name_money[np.array(most_v[1::3]).argmax()])
 print('Самая выгодный перевод на следующий квартал: ', name_money[np.array(most_v[2::3]).argmax()])
-
-
-
